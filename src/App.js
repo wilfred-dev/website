4de73e5a2c5f6bbb10f9b4ce8c9f4a7865ab8763
Vilhelm Prytz <vilhelm@prytznet.se>
2020-02-09 17:51:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+
+const Container = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  margin: 0px;
+`;
+
+const Text = styled.p`
+  margin: 0px;
+`;
+
+const IconContainer = styled.div`
+  padding-top: 0.25rem;
+`;
+
+const Icon = styled.a`
+  margin: 0.25rem;
+  font-size: 1.5rem;
+  color: white;
+  display: inline;
+  svg {
+    cursor: pointer;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Title>Wilfred</Title>
+        <Text>
+          <span role="img" aria-label="Squirrel">
+            🐿️
+          </span>{" "}
+          A CLI for managing game servers using Docker.
+        </Text>
+        <IconContainer>
+          <Icon href="https://github.com/wilfred-dev/wilfred" target="_blank">
+            <FaGithub />
+          </Icon>
+          <Icon href="https://docs.wilfredproject.org" target="_blank">
+            <FaBook />
+          </Icon>
+        </IconContainer>
+      </Container>
     </div>
   );
 }
